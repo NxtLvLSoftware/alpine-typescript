2b88673d1d114181213734447e735b7e75cbde4f
JackNoordhuis <jackjnoordhuis@gmail.com>
2023-09-02 10:31:49 +1000

    fixup

---
+++ examples/project/src/index.ts
@@ -0,0 +1,18 @@
+import './style.css';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+import { AlpineComponents } from '@nxtlvlsoftware/alpine-typescript';
+
+import {AlertComponent} from "./components/AlertComponent";
+import { ToggleComponent } from "./components/ToggleComponent";
+
+AlpineComponents.bootstrap({
+	components: {
+		alert: AlertComponent,
+		toggle: ToggleComponent
+	},
+	logErrors: true
+});
