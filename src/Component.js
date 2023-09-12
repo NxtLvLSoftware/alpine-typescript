efd98eeb85d05c326c78934cd3b314aad39a9429
JackNoordhuis <JackNoordhuis@users.noreply.github.com>
2023-09-12 09:45:22 +0000

    [create-pull-request] update tsc build dist files

---
+++ src/Component.js
@@ -0,0 +1,7 @@
+var AlpineComponent = (function () {
+    function AlpineComponent() {
+    }
+    return AlpineComponent;
+}());
+export { AlpineComponent };
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ29tcG9uZW50LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiQ29tcG9uZW50LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQTRDQTtJQUFBO0lBd0RBLENBQUM7SUFBRCxzQkFBQztBQUFELENBQUMsQUF4REQsSUF3REMifQ==
